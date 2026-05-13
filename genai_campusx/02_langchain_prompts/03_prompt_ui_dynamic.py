f8527e7fb926544c9d4a745ce6b31c22d46f9d4f
Narotam-Mishra <narotam21jan@gmail.com>
2026-05-13 15:43:26 +0530

    genai_prompt_template_segregation

--- genai_campusx/02_langchain_prompts/03_prompt_ui_dynamic.py
+++ genai_campusx/02_langchain_prompts/03_prompt_ui_dynamic.py
@@ -4,7 +4,7 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, load_prompt
 
 load_dotenv(override=True)
 
@@ -35,30 +35,14 @@ length_input = st.selectbox(
     ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"],
 )
 
-# create prompt template
-prompt_template = PromptTemplate(
-    template="""
-    Please summarize the research paper titled "{paper_input}" with the following specifications:
-    Explanation Style: {style_input}
-    Explanation Length: {length_input}
-    1. Mathematical Details:
-        - Include relevant mathematical equations if present in the paper.
-        - Explain the mathematical concepts using simple, intuitive code snippets where applicable.
-    2. Analogies:
-        - Use relatable analogies to simplify complex ideas.
-    If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.
-    Ensure the summary is clear, accurate, and aligned with the provided style and length.
-""",
-input_variables=['paper_input', 'style_input', 'length_input']
-)
+prompt_template = load_prompt('prompt_template.json')
 
-# fill the placeholders
-prompt = prompt_template.invoke({
-    'paper_input': paper_input,
-    'style_input': style_input,
-    'length_input': length_input
-})
-
+# chaining
 if st.button('Summarize'):
-    res = model.invoke(prompt)
+    chain = prompt_template | model
+    res = chain.invoke({
+        'paper_input': paper_input,
+        'style_input': style_input,
+        'length_input': length_input
+    })
     st.write(res.content)
